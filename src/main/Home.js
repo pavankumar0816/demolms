1c526f7a45385c92555decbfcb917a71033310a1
Manikanta Pavan Kumar <manikantapavankumarpuppala@gmail.com>
2024-04-30 16:16:55 +0530

    V1.0

--- src/main/Home.js
+++ src/main/Home.js
@@ -1,44 +1,21 @@
 import React from 'react';
 
-import mobapp from './images/lmsmob.png';
-import lmscourse from './images/lmscourse.png';
-import lmssubj from './images/lmssubj.png';
+// import mobapp from './images/lmsmob.png';
+// import lmscourse from './images/lmscourse.png';
+// import lmssubj from './images/lmssubj.png';
 import backgroundpng from './images/background.png'; // Importing the background image
-
 import './home.css';
 
 export default function Home() {
-  const headerStyle = {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${backgroundpng})`,
-    height: '100vh',
-    marginTop: '-1px',
-    fontSize: '50px',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    padding: '10px 8%',
-    position: 'relative',
-    resize: 'none',
-  };
 
   return (
-    <div style={headerStyle}>
+    <div className="header">
+      <img src={backgroundpng} alt="app" className="background-image" />
+      
+     
+     
 
-        {/* <div className="explore-button">
-            <button id="signInButton">Sign-in</button>
-            <div className="dropdown-content" id="signInDropdown">
-              <a href="/adminlogin">Admin Login</a>
-              <a href="/studentlogin">Student Login</a>
-              <a href="/facultylogin">Faculty Login</a>
-            </div>
-          </div> */}
-
-      <div className="header-content">
-        <h3>Student LMS</h3>
-        <h6>Learn the courses</h6>
-        {/* <p>Ready to start? Login In</p> */}
-      </div>
-
-      {/* <div className="sources">
+       {/* <div className="sources">
         <div className="row">                 
           <div className="text-col">
             <h3>Enjoy by accessing all the Content</h3>
@@ -72,7 +49,7 @@ export default function Home() {
                <img src={lmssubj} alt="app"/>
           </div>
         </div>
-      </div> */}
+      </div>  */}
     </div>
   );
 }
